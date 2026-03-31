db4cfa12a6b1a192d31b7d0d01a5d45efa08d4fc
Tanmay100000 <wadibhasmetanmay605@gmail.com>
2026-03-31 22:42:00 +0530

    Initial project setup

---
+++ session-auth/src/main/java/com/tanmay/auth/session_auth/SessionAuthApplication.java
@@ -0,0 +1,14 @@
+package com.tanmay.auth.session_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionAuthApplication.class, args);
+		System.out.println("Hello dear");
+	}
+
+}
---
+++ session-auth/src/main/java/com/tanmay/auth/session_auth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tanmay.auth.session_auth.repository;
+
+import com.tanmay.auth.session_auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
